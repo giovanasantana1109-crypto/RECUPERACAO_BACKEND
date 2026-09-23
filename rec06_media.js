119b16f77aec08732786cb9da067441c49f2f6af
giovanasantana1109-cripto <giovana.santana1109@gmail.com>
2026-09-23 16:35:28 -0300

    sexto exercicio

--- rec06_media.js
+++ rec06_media.js
@@ -3,10 +3,35 @@
 // Uma equipe registrou o tempo, em minutos, de seis atendimentos técnicos e deseja calcular a média.
 
 // O programa deve:
+// parte 1
 // ☐ Criar um acumulador iniciado em zero.
+
+// parte2
 // ☐ Usar um laço para solicitar exatamente 6 tempos.
+
+// parte3
 // ☐ Somar cada valor ao acumulador.
+
+// parte4
 // ☐ Calcular a média ao final.
+
+// parte5
 // ☐ Exibir a soma dos tempos e a média.
 
 const entrada = require("readline-sync");
+console.log('===REGISTRO DE TEMPO===')
+let acumulo = 0;
+
+for (let tempo = 0; tempo <=5; tempo++){
+    const atendimento = entrada.questionFloat(`Digite o tempo ${tempo+1}: `);
+    acumulo += atendimento;
+}
+
+const media = acumulo / 6;
+
+console.log("RESUMO DA ÓPERA");
+console.log(`SOMA: ${acumulo}`);
+console.log(`MEDIA: ${media}`);
+
+
+
---
+++ rec07_setores.js
@@ -0,0 +1,15 @@
+// EXERCÍCIO 07 - Lista de setores da fábrica	Nível 4;	10 pts
+
+// Objetivo: Cadastrar e percorrer dados armazenados em um array.
+// Crie um programa para cadastrar seis setores de uma fábrica e, ao final, listar todos os setores numerados.
+
+// O programa deve:
+// ☐ Criar um array vazio.
+// ☐ Usar um laço para solicitar 6 nomes de setores.
+// ☐ Adicionar cada nome ao array usando push().
+// ☐ Percorrer o array novamente após o cadastro.
+// ☐ Exibir no formato "1 - Montagem", "2 - Qualidade" etc.
+// ☐ Usar a propriedade length em pelo menos um dos laços.
+
+
+const entrada = require("readline-sync");
