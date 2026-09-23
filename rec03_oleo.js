669e46a95eca86f37c3f3a29dc27ab876fd2d1fc
giovanasantana1109-cripto <giovana.santana1109@gmail.com>
2026-09-23 14:28:12 -0300

    terceirio exercicio

---
+++ rec03_oleo.js
@@ -0,0 +1,31 @@
+// EXERCÍCIO 03 - Verificação de nível de óleo	Nível 2;	8 pts
+
+// Objetivo: Aplicar uma estrutura condicional simples.
+// O nível de óleo de uma máquina deve permanecer entre 40% e 80%, inclusive. Fora desse intervalo, a máquina precisa de inspeção.
+
+// O programa deve:
+// parte1
+// ☐ Solicitar o nível de óleo em porcentagem.
+
+// ☐ Usar if/else para verificar se o nível está dentro do intervalo.
+// ☐ Exibir "NÍVEL NORMAL" quando estiver entre 40 e 80.
+
+// ☐ Exibir "INSPEÇÃO NECESSÁRIA" nos demais casos.
+// ☐ Exibir também o valor informado.
+
+const entrada = require ('readline-sync');
+
+const nivelOleo = entrada.questionFloat("Digite o nivel do oleo em porcentage: ");
+
+console.log("== RELATÓRIO ==");
+console.log("  Nível de Óleo");
+
+if ( nivelOleo >= 40 && nivelOleo <= 80 ){
+    console.log(" -NÍVEL NORMAL-");
+}else {
+    console.log("-INSPEÇÃO NECESSÁRIA-");
+}
+
+
+
+
